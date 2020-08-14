bcd7dea4d69b5f77050aff39744cda3adec10060
rainleee <rainlee4401@gmail.com>
2020-08-14 21:09:35 +0900

    자바스크립트 객체화의 이해, document를 이용해 엘리먼트에 접근하는 방식

---
+++ callByElement.js
@@ -0,0 +1,25 @@
+//id를 가져와서 console로 확인해보면 html tag를 볼 수 있다.
+const title = document.getElementById("title");
+//innerHTML이기 때문에 html tag도 사용가능하다. 기존에있던 내용을 바꿀 수 있다.
+title.innerHTML = "Hi change h1 title. <br> rainlee best programmer!!";
+//title->style에서 색깔을 red로 변경
+title.style.color = "red";
+//개발자가 객체의 속성을 쉽게 얻을 수 있도록
+//콘솔에서 지정된 JavaScript 객체의 모든 속성을 보는 방법입니다. MDN참조
+console.dir(title);
+console.dir(document);
+let a = document.all;
+console.log(a);
+console.log(a[5]);
+a = a[5].innerHTML = "Hello";
+console.log(a);
+let tagName = document.getElementsByTagName(title); //h1 h2 body ...etc
+console.log(tagName);
+
+//제이쿼리로 치면 $("#title").on() 의 형식으로 쓰임.
+const titleNew = document.querySelector("#title");
+console.log(titleNew);
+
+//console.error("oooooooh Noooooo!!");
+
+//Document Object Module
--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-console.log("hello world");
+//자바스크립트 function 만들어보기
+function sayHello(name, age) {
+  //기존에 java에서 하던방식
+  //console.log("Hello " + name + " you are " + age + " years old");
 
-let a = 221;
-let b = a - 5;
-//console.log(b);
+  //(₩)백틱을 이용한 방법
+  //console.log(`Hello ${name} you are ${age} years old`);
 
-const temp = 200;
-console.log(temp);
+  //console.log()는 console에만 보여질뿐 이 function이 어떤값을 return하는지 정해지지 않음.
+  return `Hello ${name} you are ${age} years old`;
+}
+//return값 없이 console.log를 찍으면  undefined가 뜬다.
+let greetRainlee = sayHello("rianlee", 20);
+
+//console.log(greetRainlee);
+
+const calculator = {
+  plus: function (num1, num2) {
+    return num1 + num2;
+  },
+  minus: function (num1, num2) {
+    return num1 - num2;
+  },
+  multiplication: function (num1, num2) {
+    return num1 * num2;
+  },
+};
+
+const plus = calculator.plus(5, 5);
+const minus = calculator.minus(10, 5);
+const multiplication = calculator.multiplication(3, 3);
+
+console.log(plus);
+console.log(minus);
+console.log(multiplication);
+// ,를 사용할경우 각각의 값을 나타낼 수 있다.
+console.log(plus, minus, multiplication);
+//연산자를 이용하면 변수끼리 연산처리를 해서 출력할 수 있다.
+console.log(plus + minus);
